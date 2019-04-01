a4bf2fa3861aff25166d2bb2841a3b62997ee8ad
Danielgiraldo <danielgiraldoch@gmail.com>
2019-04-01 14:38:58 -0500

    componente artista redireccionamiento

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -14,7 +14,7 @@ export class SpotifyService {
   getQuery(query: string) {
     const url = `https://api.spotify.com/v1/${query}`;
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQDaiQE2YQvcF4FZEARxZT2RtpQmN1dgicu0DaYbeCqKwXV6Niz6zQCGVh2IlZNXVCNs4GVOxp2WCpi48hA'
+      'Authorization': 'Bearer BQBGcBoRHXEvryvETwI_qhjJ-j9-VL-pU4z61mH-WZf64M-CSVOCDRHZzkb9akQtPgI8BfY1RREI-evih_4'
     });
 
     return this.http.get(url, { headers });
