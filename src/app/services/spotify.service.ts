d29c1f45cbbe02b68319004247eb8ade2a63367f
Danielgiraldo <danielgiraldoch@gmail.com>
2019-04-01 15:40:33 -0500

    Favicon, artista busqueda y redirección y loading

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -14,7 +14,7 @@ export class SpotifyService {
   getQuery(query: string) {
     const url = `https://api.spotify.com/v1/${query}`;
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQBGcBoRHXEvryvETwI_qhjJ-j9-VL-pU4z61mH-WZf64M-CSVOCDRHZzkb9akQtPgI8BfY1RREI-evih_4'
+      'Authorization': 'Bearer BQCEgmXPT4rybza9f3o-PzAAIwIxutbirPXq3pFZQdY6Ux8zVkta1Xc4KigcTQgRLxex4k9NhkGA0NJISnI'
     });
 
     return this.http.get(url, { headers });
@@ -25,8 +25,12 @@ export class SpotifyService {
       .pipe(map(data => data['albums'].items));
   }
 
-  getArtista(termino: string) {
+  getArtistas(termino: string) {
     return this.getQuery(`search?q=${termino}&type=artist&limit=15`)
       .pipe(map(data => data['artists'].items));
+  }
+  getArtista(id: string) {
+    return this.getQuery(`artists/${ id }`);
+      // .pipe(map(data => data['artists'].items));
   }
 }
